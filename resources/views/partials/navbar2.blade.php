2bddd5372bbb5f9b770922759a5fe2c5c406958b
Nuzulurrahmah <nuzulurrahmah3899@gmail.com>
2023-05-04 04:02:51 +0700

    docs: modify some file

--- resources/views/partials/navbar2.blade.php
+++ resources/views/partials/navbar2.blade.php
@@ -1,6 +1,6 @@
 <!-- repair this code -->
 <header class="relative inset-x-0 bottom-0">
-    <div class="flex inset-x-0 relative w-auto justify-between relative bg-purple-500 shadow-xl">
+    <div class="flex inset-x-0 relative w-auto justify-between bg-purple-500 shadow-xl">
         <div class="px-4">
             <a href="/dashboard" class="font-bold text-lg text-primary block py-1">
                 <img src="{{ asset('image/logo.png') }}" alt="logo" class="w-20">
@@ -14,8 +14,7 @@
                 <span class="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
             </button>
 
-            <nav id="nav-menu"
-                class="hidden relative inset-x-0 w-auto py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none  ">
+            <nav id="nav-menu" class="hidden relative inset-x-0 w-auto py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none  ">
                 <ul class="block lg:flex ">
 
 
@@ -24,8 +23,7 @@
                         <a href="/login" class="text-base text-black py-2 mx-8 flex group-hover:text-primary">Login</a>
                     </li>
                     <li class="group">
-                        <a href="/register"
-                            class="text-base text-black py-2 mx-8 flex group-hover:text-primary">Register</a>
+                        <a href="/register" class="text-base text-black py-2 mx-8 flex group-hover:text-primary ">Register</a>
                     </li>
 
 
