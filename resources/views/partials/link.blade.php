4a792e27b3a314d1d15cefbf02edfb51be862997
Nuzulurrahmah <nuzulurrahmah3899@gmail.com>
2023-05-09 08:58:31 +0700

    docs: add needed link

--- resources/views/partials/link.blade.php
+++ resources/views/partials/link.blade.php
@@ -12,3 +12,4 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
+<link href="'https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet"/>
